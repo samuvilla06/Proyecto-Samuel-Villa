50546486253be0028481fa26e5e0ceb3b351bb2d
samuvilla06 <tabaressamuel22@gmail.com>
2024-05-29 20:54:02 -0500

    configuración inicil server npm run dev

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = require('./app');
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
